574c3fe6ac2516eb385a98628ebcba3b02b929d7
MichalKrzyzak <michalkrzyzak95@gmail.com>
2019-04-21 13:25:52 +0200

    Created reference variable of Sort

--- src/UserChoice.java
+++ src/UserChoice.java
@@ -7,20 +7,21 @@ import java.util.*;
 
 class UserChoice {
     void verifyUserChoice(String userChoice, Scanner userInput) {
+        Sort sort = new Sort();
         List<Integer> listOfNumbers = new ArrayList<>();
 
         if (userChoice.equals("1")) {
             System.out.println("Please enter your numbers dividing them by \",\"");
             String userNumbers = userInput.nextLine();
             listOfNumbers = splitUserNumbers(userNumbers);
-            new Sort().sortNumbers(listOfNumbers);
+            sort.sortNumbers(listOfNumbers);
         } else if (userChoice.equals("2")) {
             Random random = new Random();
             for (int i = 0; i < random.nextInt((100) + 1); i++) {
                 listOfNumbers.add(random.nextInt(9999));
             }
             System.out.println("Generated numbers: " + String.valueOf(listOfNumbers));
-            new Sort().sortNumbers(listOfNumbers);
+            sort.sortNumbers(listOfNumbers);
         } else if (userChoice.equalsIgnoreCase("exit") || userChoice.equalsIgnoreCase("q")) {
             System.out.println("Exiting...");
             Thread.interrupted();
